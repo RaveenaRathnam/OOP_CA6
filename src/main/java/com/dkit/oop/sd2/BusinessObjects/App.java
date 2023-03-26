388349d5b9b1dfcfc8cb0443893241850286cb41
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-27 00:19:02 +0100

    feature-1 fully functional

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/App.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/App.java
@@ -27,7 +27,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        ArtistDaoInterface IUserDao = new MySqlArtistDao();  //"IUserDao" -> "I" stands for for
+        ArtistDaoInterface IArtistDao = new MySqlArtistDao();  //"IUserDao" -> "I" stands for for
 
 //        // Notice that the userDao reference is an Interface type.
 //        // This allows for the use of different concrete implementations.
@@ -47,75 +47,75 @@ public class App
         try
         {
             System.out.println("\nCall findAllUsers()");
-            List<Artist> artists = IUserDao.findAllUsers();     // call a method in the DAO
+            List<Artist> artists = IArtistDao.findAllArtists();     // call a method in the DAO
 
             if( artists.isEmpty() )
-                System.out.println("There are no Users");
+                System.out.println("There are no Artists");
             else {
                 for (Artist artist : artists)
-                    System.out.println("User: " + artist.toString());
+                    System.out.println("Artist: " + artist.toString());
             }
 
-            // test dao - with username and password that we know are present in the database
-            System.out.println("\nCall: findUserByUsernamePassword()");
-            String username = "smithj";
-            String password = "password";
-            Artist artist = IUserDao.findUserByUsernamePassword(username, password);
-
-            if( artist != null ) // null returned if userid and password not valid
-                System.out.println("User found: " + artist);
-            else
-                System.out.println("Username with that password not found");
-
-            // test dao - with an invalid username (i.e. not in database)
-            username = "madmax";
-            password = "thunderdome";
-            artist = IUserDao.findUserByUsernamePassword(username, password);
-            if(artist != null)
-                System.out.println("Username: " + username + " was found: " + artist);
-            else
-                System.out.println("Username: " + username + ", password: " + password +" is not valid.");
-
-            //test-class findAllUsersLastNameContains()
-            System.out.println("\nCall findAllUsersLastNameContains()");
-            String substring="S";
-            List<Artist> usersContainln = IUserDao.findAllUsersLastNameContains(substring);     // call a method in the DAO
-
-            if( artists.isEmpty() )
-                System.out.println("There are no Users");
-            else {
-                for (Artist artist1 : usersContainln)
-                    System.out.println("User: " + artist1.toString());
-            }
-
-            //test-class addUser()
-            System.out.println("\nCall findAllUsers()");
-            Artist artist2 = IUserDao.addUser(new Artist("Mary","Ann","maryann","mann123"));     // call a method in the DAO
-            System.out.println("User: " + artist2.toString());
-
-            System.out.println("\nCall findAllUsers()");
-             artists = IUserDao.findAllUsers();     // call a method in the DAO
-
-            if( artists.isEmpty() )
-                System.out.println("There are no Users");
-            else {
-                for (Artist artist1 : artists)
-                    System.out.println("User: " + artist1.toString());
-            }
-            //test-updatepassword
-            System.out.println("\nCall UpdatePassword()");
-            Artist artist3Z = IUserDao.addUser(new Artist("Mary","Ann","maryann","mann123"));     // call a method in the DAO
-            System.out.println("User: " + artist2.toString());
-
-            System.out.println("\nCall findAllUsers()");
-            artists = IUserDao.findAllUsers();     // call a method in the DAO
-
-            if( artists.isEmpty() )
-                System.out.println("There are no Users");
-            else {
-                for (Artist artist1 : artists)
-                    System.out.println("User: " + artist1.toString());
-            }
+//            // test dao - with username and password that we know are present in the database
+//            System.out.println("\nCall: findArtistByUsernamePassword()");
+//            String username = "smithj";
+//            String password = "password";
+//            Artist artist = IUserDao.findUserByUsernamePassword(username, password);
+//
+//            if( artist != null ) // null returned if userid and password not valid
+//                System.out.println("User found: " + artist);
+//            else
+//                System.out.println("Username with that password not found");
+//
+//            // test dao - with an invalid username (i.e. not in database)
+//            username = "madmax";
+//            password = "thunderdome";
+//            artist = IUserDao.findUserByUsernamePassword(username, password);
+//            if(artist != null)
+//                System.out.println("Username: " + username + " was found: " + artist);
+//            else
+//                System.out.println("Username: " + username + ", password: " + password +" is not valid.");
+//
+//            //test-class findAllUsersLastNameContains()
+//            System.out.println("\nCall findAllUsersLastNameContains()");
+//            String substring="S";
+//            List<Artist> usersContainln = IUserDao.findAllUsersLastNameContains(substring);     // call a method in the DAO
+//
+//            if( artists.isEmpty() )
+//                System.out.println("There are no Users");
+//            else {
+//                for (Artist artist1 : usersContainln)
+//                    System.out.println("User: " + artist1.toString());
+//            }
+//
+//            //test-class addUser()
+//            System.out.println("\nCall findAllUsers()");
+//            Artist artist2 = IUserDao.addUser(new Artist("Mary","Ann","maryann","mann123"));     // call a method in the DAO
+//            System.out.println("User: " + artist2.toString());
+//
+//            System.out.println("\nCall findAllUsers()");
+//             artists = IUserDao.findAllUsers();     // call a method in the DAO
+//
+//            if( artists.isEmpty() )
+//                System.out.println("There are no Users");
+//            else {
+//                for (Artist artist1 : artists)
+//                    System.out.println("User: " + artist1.toString());
+//            }
+//            //test-updatepassword
+//            System.out.println("\nCall UpdatePassword()");
+//            Artist artist3Z = IUserDao.addUser(new Artist("Mary","Ann","maryann","mann123"));     // call a method in the DAO
+//            System.out.println("User: " + artist2.toString());
+//
+//            System.out.println("\nCall findAllUsers()");
+//            artists = IUserDao.findAllUsers();     // call a method in the DAO
+//
+//            if( artists.isEmpty() )
+//                System.out.println("There are no Users");
+//            else {
+//                for (Artist artist1 : artists)
+//                    System.out.println("User: " + artist1.toString());
+//            }
         }
         catch( DaoException e )
         {
