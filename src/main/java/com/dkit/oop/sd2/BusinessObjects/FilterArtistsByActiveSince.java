d43e78dc1236f36ad78fc31dd9d2f9954c28cbf9
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-30 16:16:20 +0100

    Updated FilterArtistsByActiveSince.java
    added the code to filter artists by active since .i.e. the year they have been established .This filter works with a min and a max range.

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/FilterArtistsByActiveSince.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/FilterArtistsByActiveSince.java
@@ -1,4 +1,21 @@
 package com.dkit.oop.sd2.BusinessObjects;
 
-public class FilterArtistsByActiveSince {
+import com.dkit.oop.sd2.DTOs.Artist;
+
+public class FilterArtistsByActiveSince implements IFilter{
+    private int minActiveSince, maxActiveSince;
+
+    public FilterArtistsByActiveSince(int minActiveSince, int maxActiveSince)
+    {
+        this.minActiveSince = minActiveSince;
+        this.maxActiveSince= maxActiveSince;
+    }
+
+    @Override
+    public boolean matches(Object object)
+    {
+        Artist a = (Artist) object;
+
+        return a.getActive_since() >= minActiveSince && a.getActive_since() <=maxActiveSince;
+    }
 }
