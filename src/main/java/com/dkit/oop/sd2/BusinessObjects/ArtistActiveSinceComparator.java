44a6caf333818c3308ace127f3df73d230829bf3
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-30 19:33:41 +0100

    Updated ArtistActiveSinceComparator.java
    added the code to compare two artists by active_since filed.

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/ArtistActiveSinceComparator.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/ArtistActiveSinceComparator.java
@@ -1,4 +1,26 @@
 package com.dkit.oop.sd2.BusinessObjects;
 
-public class ArtistActiveSinceComparator {
+import com.dkit.oop.sd2.DTOs.Artist;
+
+import java.util.Comparator;
+
+public class ArtistActiveSinceComparator implements Comparator<Artist> {
+    // implement the compare() method required by the Comparator interface
+    public int compare(Artist a1, Artist a2) {
+        if (a1.getActive_since() < a2.getActive_since()) {
+            return -1;    // a negative value
+        } else if (a1.getActive_since() == a2.getActive_since()) {
+            return 0;
+        } else {
+            return 1;
+        }
+
+        // Note that we must use getActive_since() above, as the active_since field
+        // is private to the Artist class.
+
+//		As an alternative to the above, we could subtract one from
+//		the other and return the difference.
+//		return (a1.getActive_since() - a2.getActive_since());
+
+    }
 }
