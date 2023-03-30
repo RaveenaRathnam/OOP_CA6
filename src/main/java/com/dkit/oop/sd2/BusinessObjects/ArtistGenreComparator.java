d60853cebfd4ca10db48cbe249704d6b9aafcc29
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-30 19:26:18 +0100

    Updated ArtistGenreComparator.java
    added the code to compare two artists by genre.

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/ArtistGenreComparator.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/ArtistGenreComparator.java
@@ -1,4 +1,17 @@
 package com.dkit.oop.sd2.BusinessObjects;
 
-public class ArtistGenreComparator {
+import com.dkit.oop.sd2.DTOs.Artist;
+
+import java.util.Comparator;
+
+public class ArtistGenreComparator implements Comparator<Artist> {
+    // implement the compare() method required by the Comparator interface
+    public int compare(Artist a1, Artist a2) {
+        return a1.getGenre().compareTo(a2.getGenre());
+
+        // The Genre of a ARTIST is of type String and the String class implements
+        // a compareTo() method that returns -1, 0, or +1 as appropriate.
+        // So, to compare string fields we simply use the compareTo() method
+        // of the String class.
+    }
 }
