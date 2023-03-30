74a80df805e909e0c624f65d4677707d6f59deda
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-30 19:23:37 +0100

    Updated the ArtistNameComparator.java
    added the code to compare two artists by name.

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/ArtistNameComparator.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/ArtistNameComparator.java
@@ -1,4 +1,17 @@
 package com.dkit.oop.sd2.BusinessObjects;
 
-public class ArtistNameComparator {
+import com.dkit.oop.sd2.DTOs.Artist;
+
+import java.util.Comparator;
+public class ArtistNameComparator implements Comparator<Artist> {
+    // implement the compare() method required by the Comparator interface
+    public int compare(Artist a1, Artist a2)
+    {
+        return a1.getName().compareTo(a2.getName());
+
+        // The Name of a ARTIST is of type String and the String class implements
+        // a compareTo() method that returns -1, 0, or +1 as appropriate.
+        // So, to compare string fields we simply use the compareTo() method
+        // of the String class.
+    }
 }
