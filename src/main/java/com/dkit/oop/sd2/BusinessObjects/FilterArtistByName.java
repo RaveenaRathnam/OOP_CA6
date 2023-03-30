7767bc6a5d208b5ee3e12af98ace2de994b3d143
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-30 15:54:05 +0100

    Updated FilterArtistByName.java
    Added the code to filter artists by name.

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/FilterArtistByName.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/FilterArtistByName.java
@@ -1,4 +1,17 @@
 package com.dkit.oop.sd2.BusinessObjects;
 
-public class FilterArtistByName {
+import com.dkit.oop.sd2.DTOs.Artist;
+
+public class FilterArtistByName implements IFilter{
+    private String name;
+
+    public FilterArtistByName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean matches(Object other) {
+        Artist a = (Artist) other;        // cast from Object to Artist
+        return a.getName().equalsIgnoreCase(name);
+    }
 }
