b66648a846ffabb1dc14ba937655230997655777
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-30 19:30:37 +0100

    Updated to ArtistRatingComparator.java
    added the code to compare two artists by rating.

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/ArtistRatingComparator.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/ArtistRatingComparator.java
@@ -1,4 +1,26 @@
 package com.dkit.oop.sd2.BusinessObjects;
 
-public class ArtistRatingComparator {
+import com.dkit.oop.sd2.DTOs.Artist;
+
+import java.util.Comparator;
+
+public class ArtistRatingComparator implements Comparator<Artist> {
+    // implement the compare() method required by the Comparator interface
+    public int compare(Artist a1, Artist a2) {
+        if (a1.getRating() < a2.getRating()) {
+            return -1;	// a negative value
+        } else if (a1.getRating() == a2.getRating()) {
+            return 0;
+        } else {
+            return 1;
+        }
+
+        // Note that we must use getRating() above, as the rating field
+        // is private to the Artist class.
+
+//		As an alternative to the above, we could subtract one from
+//		the other and return the difference.
+//		return (a1.getRating() - a2.getRating());
+
+    }
 }
