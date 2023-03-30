c1c44955a72216b68c8955ea4060ae7a3f37f5f4
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-30 15:47:44 +0100

    Added the code to filter artists by genre in FilterArtistsByGenre.java

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/FilterArtistsByGenre.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/FilterArtistsByGenre.java
@@ -1,4 +1,18 @@
 package com.dkit.oop.sd2.BusinessObjects;
 
-public class FilterArtistsByGenre {
+import com.dkit.oop.sd2.DTOs.Artist;
+
+public class FilterArtistsByGenre implements IFilter {
+
+        private String genre;
+
+        public FilterArtistsByGenre(String genre) {
+            this.genre = genre;
+        }
+
+        @Override
+        public boolean matches(Object other) {
+            Artist a = (Artist) other;        // cast from Object to Artist
+            return a.getGenre().equalsIgnoreCase(genre);
+        }
 }
