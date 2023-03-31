666e48547acfbff921241c2aca9718953fafbb19
Raveena Rathnam <D00251856@student.dkit.ie>
2023-03-31 14:47:51 +0100

    Updated ArtistCache.java
    added the field and the required methods for the ArtistCache class

--- src/main/java/com/dkit/oop/sd2/Cache/ArtistCache.java
+++ src/main/java/com/dkit/oop/sd2/Cache/ArtistCache.java
@@ -1,4 +1,22 @@
 package com.dkit.oop.sd2.Cache;
 
+import java.util.HashSet;
+
 public class ArtistCache {
+    private HashSet<Integer> artistIds;
+
+    public ArtistCache(){
+        this.artistIds=new HashSet<>();
+    }
+    public boolean contains(int artistId){
+        return artistIds.contains(artistId);
+    }
+    public void add(int  artistId) {
+        artistIds.add(artistId);
+    }
+
+    public void remove(int artistId) {
+        artistIds.remove(artistId);
+    }
+
 }
