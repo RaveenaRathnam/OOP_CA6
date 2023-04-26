b38330a89e8b63a96438fe2e8b6ef4d48e73129c
Raveena Rathnam <D00251856@student.dkit.ie>
2023-04-26 02:24:28 +0100

    Updated client.java
    added the code to create a list of artist object from the json obtained from the server to display all the artists.

--- src/main/java/com/dkit/oop/sd2/Client/Client.java
+++ src/main/java/com/dkit/oop/sd2/Client/Client.java
@@ -19,10 +19,20 @@ package com.dkit.oop.sd2.Client;
  * (Both the server and the client will be running together on this computer)
  */
 
+import com.dkit.oop.sd2.BusinessObjects.JsonDeserializerArtist;
+import com.dkit.oop.sd2.DTOs.Artist;
+import com.dkit.oop.sd2.Exceptions.DaoException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Client {
@@ -37,7 +47,7 @@ public class Client {
         {
             Scanner in = new Scanner(System.in);
             try {
-                Socket socket = new Socket("192.168.178.69", 8080);  // connect to server socket
+                Socket socket = new Socket("192.168.14.13", 8080);  // connect to server socket
                 System.out.println("Client: Port# of this client : " + socket.getLocalPort());
                 System.out.println("Client: Port# of Server :" + socket.getPort() );
 
@@ -55,7 +65,8 @@ public class Client {
                     System.out.println("Please enter the Id of the Artist: ");
                     int artistId=in.nextInt();
                     socketWriter.println(command+" "+artistId);
-                } else if (command.startsWith("2")) {
+               }
+                else if (command.startsWith("2")) {
                     socketWriter.println(command);
                 }
 
@@ -64,18 +75,25 @@ public class Client {
                 if(command.startsWith("1"))   //we expect the server to return a time
                 {
                     String artistByIdString = socketReader.nextLine();
-                    System.out.println("Client message: Displaying Artist By ID: " + artistByIdString );
+                    Artist artist=artistJsonToGson(artistByIdString);
+                    System.out.println("Client message: Displaying Artist By ID: " + artist );
                 }
                 else if(command.startsWith("2"))
                 {
-                    String artistsString = socketReader.nextLine();
-                    System.out.println("Client message: Displaying All Artists: " + artistsString );
+                    System.out.println("Client message: Displaying All Artists: ");
+                    String artistsStringJson= socketReader.nextLine();
+                    Artist[] artists = new Gson().fromJson(artistsStringJson, Artist[].class);
+                    List<Artist> artistList = new ArrayList<>(Arrays.asList(artists));
+                    for(Artist a:artistList){
+                        System.out.println(a);
+                    }
+
                 }
-//                else                            // the user has entered the Echo command or an invalid command
-//                {
-//                    String input = socketReader.nextLine();
-//                    System.out.println("Client message: Response from server: \"" + input + "\"");
-//                }
+                else                            // the user has entered the Echo command or an invalid command
+                {
+                    String input = socketReader.nextLine();
+                    System.out.println("Client message: Response from server: \"" + input + "\"");
+                }
 
                 socketWriter.close();
                 socketReader.close();
@@ -83,7 +101,12 @@ public class Client {
 
             } catch (IOException e) {
                 System.out.println("Client message: IOException: "+e);
+            } catch (DaoException e) {
+                throw new RuntimeException(e);
             }
+//            catch (DaoException e) {
+//                throw new RuntimeException(e);
+//            }
         }
     private static void printMenuInstructions() {
         System.out.println("======================================  WELCOME ! ==============================");
@@ -98,7 +121,31 @@ public class Client {
         System.out.println("|   -------------------------------------------------------------------------  |");
         System.out.println("================================================================================");
     }
+    public Artist artistJsonToGson(String artist) throws DaoException
+    {
+        if (artist == null) {
+            System.out.println("Connection failed, exiting.");
+            return null;
+        }
+        // Instantiate a GsonBuilder and register the TypeAdapter (to adapt types!)
+        // passing in the IssPositionAtTime class definition,
+        // the name of the deserialization object (containing the deserialize() method)
+        //
+        Gson gsonBuilder = new GsonBuilder()
+                .registerTypeAdapter(Artist.class,
+                        new JsonDeserializerArtist())
+                .serializeNulls()
+                .create();
+
+        // Now that we have set up the Adapter, we call the fromJson() method
+        // to parse the JSON string and create and populate
+        // the Java IssPositionAtTime object.
+        //
+        Artist a= gsonBuilder.fromJson(artist, new TypeToken<Artist>(){}.getType());
+        return a;
     }
+
+}
 
 
 //  LocalTime time = LocalTime.parse(timeString); // Parse timeString -> convert to LocalTime object if required
